62be4800f6c1d69541538caff4975b09351900f1
Jubril Aderomilehin <Jubril@Jubrils-MacBook-Air.local>
2020-07-22 20:43:02 -0400

    Completed lab 9

--- src/main/java/com/codedifferently/bankaccountlab/MainApplication.java
+++ src/main/java/com/codedifferently/bankaccountlab/MainApplication.java
@@ -1,4 +1,8 @@
 package com.codedifferently.bankaccountlab;
 
 public class MainApplication {
+    public static void main(String [] args){
+        BankingApplication app = new BankingApplication();
+        app.run();
+    }
 }
--- src/test/java/com/codedifferently/bankaccountlab/BankAccountTest.java
+++ src/test/java/com/codedifferently/bankaccountlab/BankAccountTest.java
@@ -1,4 +1,9 @@
 package com.codedifferently.bankaccountlab;
+import org.junit.Test;
+import org.junit.Assert;
+
+
 
 public class BankAccountTest {
+
 }
